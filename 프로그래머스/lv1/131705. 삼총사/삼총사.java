d76e782c4d73411b2ec9dc7c3a1059debabd7dd9
김혁 <witchicken2@gmail.com>
2023-05-15 09:03:06 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,21 +1,15 @@
-import java.util.*;
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        int[] temp = new int[3];
-        for(int i=0; i<number.length; i++){
-            for(int j=0; j<number.length; j++){
-                for(int k = 0; k<number.length; k++){
-                    if( i == j || i == k || j == k){
-                        continue;
-                    }
-                    if(number[i] + number[j] + number[k] == 0){
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
                         answer++;
                     }
                 }
             }
         }
-        answer /= 6;
         return answer;
     }
 }
