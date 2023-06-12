5df362ac7dc29ea74c3c32609d026ebc2f3d1302
김혁 <witchicken2@gmail.com>
2023-06-13 08:04:47 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+import math;
+def solution(price):
+    if price >= 500000 :
+        return math.floor(price * 0.8);
+    if price >= 300000 :
+        return math.floor(price * 0.9);
+    if price >= 100000 :
+        return math.floor(price * 0.95);
+    return price
