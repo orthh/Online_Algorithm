3d8f241a9c8258b7f59a6a56d576a242fc1f479c
김혁 <witchicken2@gmail.com>
2024-01-11 07:42:50 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 백준 형식 테스트
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        bw.write(String.valueOf(a+b));
+        bw.flush();
+        bw.close();
+    }
+}
