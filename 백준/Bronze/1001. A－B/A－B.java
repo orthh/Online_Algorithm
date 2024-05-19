941c5551d39471c0ce567a73e96bc503462dd7c3
김혁 <witchicken2@gmail.com>
2024-05-19 16:53:02 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //input
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        //logic
+        int answer = first - second;
+
+        //output
+        System.out.println(answer);
+    }
+}
